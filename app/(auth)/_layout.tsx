e2a7f23ccd2a00d7e5c0fb7d2050b2f3305c328e
Abdul-Wahab08 <abdulwahab3929706@gmail.com>
2026-09-16 19:56:18 +0500

    first commit

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,17 @@
+import { useAuth } from "@clerk/expo";
+import { Redirect, Stack } from "expo-router";
+
+const _layout = () => {
+
+    const { isSignedIn, isLoaded } = useAuth();
+
+    if (!isLoaded) return null;
+
+    if (isSignedIn) <Redirect href="/" />
+
+    return (
+        <Stack screenOptions={{ headerShown: false }} />
+    )
+}
+
+export default _layout
---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const signIn = () => {
+  return (
+    <View className='flex-1 justify-center items-center text-red-700'>
+      <Text>signin</Text>
+    </View>
+  )
+}
+
+export default signIn
